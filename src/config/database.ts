b6a4e6aae93f0dd07ea75bc350a8186447928285
Alan <alan.jordy1999@gmail.com>
2025-03-19 13:11:39 +0100

    add docker compose file

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,11 +3,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
-  username: 'database',
-  password: 'password',
-  database: 'database',
+  username: "root",
+  password: "password",
+  database: "donjon",
   entities: ["src/entities/*.ts"],
   synchronize: true,
   logging: true,
